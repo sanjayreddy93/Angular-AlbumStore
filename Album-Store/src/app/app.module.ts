47b4785280aed5b59a6109f1f7afaf33203c2e64
Beerelli <sanjaykumar.beerelli@accenture.com>
2020-03-10 15:35:48 +0100

    Added Product components and services

--- Album-Store/src/app/app.module.ts
+++ Album-Store/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlbumListComponent } from './components/album-list/album-list.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { ProductDescriptionComponent } from './components/product-description/product-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumListComponent,
+    ProductPageComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule
